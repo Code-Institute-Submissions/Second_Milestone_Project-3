1146f37b37ff49c1ec03441be5158134a0a14303
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-06-13 20:48:11 +0000

    JSON File Updated, Google Map JS Template Logic For Location Page Started

--- assets/script/googleMap.js
+++ assets/script/googleMap.js
@@ -2,15 +2,28 @@ var places, infoWindow;
 var mainMarker = [];
 var nearbyPlaces = [];
 var autocomplete;
-var countryRestrict = {'country': 'IS'};
+var countryRestrict = { 'country': 'IS' };
 
- $('#article-container-onbtnclick').load('attractions.html #attractions-articles');
 
+var articleTemplate = document.getElementById('article-template').content;
+var articlesContainer = document.getElementById('articles-container');
+
+var templateHtmlCopy = document.importNode(articleTemplate, true);
+
+function importArticles(templateHtmlCopy, articlesContainer) {
+    
+    console.log(templateHtmlCopy);
+    $.getJSON("assets/ajax/articles.json", function (data) {
+       console.log(data);
+    });
+}
+
+importArticles (templateHtmlCopy, articlesContainer);
 //Initializing fucntion
 function initialize() {
     initMap();
     initAutocomplete();
-  }
+}
 
 function initMap() {
 
@@ -28,7 +41,7 @@ function initMap() {
         });
     var selectType = [];
 
-    $("select.select-box").change(function(){
+    $("select.select-box").change(function () {
         selectType = [];
         var typeVal = $(this).children("option:selected").val();
         selectType.push(typeVal)
@@ -36,7 +49,7 @@ function initMap() {
     });
 
     var geocoder = new google.maps.Geocoder(map);
-    
+
     document.getElementById('src-btn').addEventListener('click', function () {
         deleteMarkers();
         geocodeAddress(geocoder, map, selectType[0]);
@@ -54,11 +67,11 @@ function geocodeAddress(geocoder, resultsMap, placeType) {
 
             var lat = results[0].geometry.location.lat();
             var lng = results[0].geometry.location.lng();
-            var marker = new google.maps.Marker({ map: resultsMap, position: results[0].geometry.location});
+            var marker = new google.maps.Marker({ map: resultsMap, position: results[0].geometry.location });
             mainMarker.push(marker);
             // Changes the innerHTML of div element to location searched and adds <ul> 
-            document.getElementById('locationName').innerHTML = '<div class="row">'  + '<div class="col-12">'  + '<h2 class="text-center mt-3 mt-lg-1">' + address + '</h2>' + '</div>' + '</div>' + '<div class="row">'  + '<div class="col-12" id="right-panel">' + '<ul id="places" class="places-list-style"></ul>' + "</div>" + "</div>";
-            findNearbyPlaces (lat, lng, resultsMap, placeType);
+            document.getElementById('locationName').innerHTML = '<div class="row">' + '<div class="col-12">' + '<h2 class="text-center mt-3 mt-lg-1">' + address + '</h2>' + '</div>' + '</div>' + '<div class="row">' + '<div class="col-12" id="right-panel">' + '<ul id="places" class="places-list-style"></ul>' + "</div>" + "</div>";
+            findNearbyPlaces(lat, lng, resultsMap, placeType);
         } else {
             alert('Geocode was not successful for the follwoing reason:' + status);
         }
@@ -69,23 +82,23 @@ function initAutocomplete() {
 
     autocomplete = new google.maps.places.Autocomplete(
         document.getElementById('address'),
-        {types: ['(cities)'], componentRestrictions: countryRestrict}
+        { types: ['(cities)'], componentRestrictions: countryRestrict }
     );
     autocomplete.setFields(['address_component']);
-  }
+}
 
-function findNearbyPlaces (lat, lng, map, placeType) {
-    var locationCoord = new google.maps.LatLng(lat,lng);
-    var request = { location: locationCoord, radius: '500', type: placeType};
+function findNearbyPlaces(lat, lng, map, placeType) {
+    var locationCoord = new google.maps.LatLng(lat, lng);
+    var request = { location: locationCoord, radius: '500', type: placeType };
     var service = new google.maps.places.PlacesService(map);
 
-    service.nearbySearch(request, function(results, status) {
+    service.nearbySearch(request, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
-                createMarker(results[i], map,);
+                createMarker(results[i], map);
             }
         }
-});
+    });
 
 }
 
@@ -99,48 +112,48 @@ function createMarker(place, map) {
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25)
-      };
+    };
 
     var placesMarker = new google.maps.Marker({
-      map: map,
-      title: place.name,
-      position: placeLoc,
-      icon: image
+        map: map,
+        title: place.name,
+        position: placeLoc,
+        icon: image
     });
 
     nearbyPlaces.push(placesMarker);
     var li = document.createElement('li');
-          li.textContent = place.name;
-          placesList.appendChild(li);
-  }
+    li.textContent = place.name;
+    placesList.appendChild(li);
+}
 
 
 function setMapOnAll(map) {
     for (var i = 0; i < nearbyPlaces.length; i++) {
         nearbyPlaces[i].setMap(map);
     }
-    for (var i = 0; i < mainMarker.length; i++){
+    for (var i = 0; i < mainMarker.length; i++) {
         mainMarker[i].setMap(map);
     }
-  }
+}
 
-  // Removes the markers from the map, but keeps them in the array.
+// Removes the markers from the map, but keeps them in the array.
 function clearMarkers() {
     setMapOnAll(null);
-  }
+}
 
-  // Shows any markers currently in the array.
+// Shows any markers currently in the array.
 function showMarkers() {
     setMapOnAll(map);
-  }
+}
 
-  // Deletes all markers in the array by removing references to them.
+// Deletes all markers in the array by removing references to them.
 function deleteMarkers() {
     clearMarkers();
     mainMarker = [];
-    nearbyPlaces= [];
-  }
+    nearbyPlaces = [];
+}
 
-  function getArticles(){
-  
-  };
+function getArticles() {
+
+};
