39a3f59d72bd4d229ae515d7477ff23fb600e173
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-06-16 22:29:24 +0000

    Added Intersection Observer API To Add Fade In Effect

---
+++ assets/script/intersectionObserver.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+let observerOptions = {
+    root: null,
+    threshold: 0.3
+};
+
+let allFadeIn = document.querySelectorAll('.fade-in');
+
+let observer = new IntersectionObserver (function( entries,observer){
+entries.forEach(entry => {
+    if (!entry.isIntersecting){
+        return;
+    }else{
+        entry.target.classList.add('appear');
+        observer.unobserve(entry.target);
+    }
+});
+
+}, observerOptions);
+
+allFadeIn.forEach(fader => {
+    observer.observe(fader);
+})
+});
